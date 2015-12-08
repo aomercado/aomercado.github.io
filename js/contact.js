6d5cd6009e40b7fdec3851f9271b7231e76930e8
Matheus Pereira <matheper@gmail.com>
2015-12-08 11:54:04 -0200

    Add post for contact.

---
+++ js/contact.js
@@ -0,0 +1,18 @@
+$(function() {
+    $('form#contact').on('submit', function (e) {
+        e.preventDefault();
+        var formData = JSON.stringify(
+            {'mail': $(this).find('#mail').val()}
+        );
+        $.ajax({
+            type: 'POST',
+            url: 'http://aomercado-matheper.rhcloud.com/contact',
+            contentType: "application/json",
+            dataType: 'json',
+            data: formData,
+            success: function(json) {
+                console.log("OK");
+            },
+        });
+    });
+});
