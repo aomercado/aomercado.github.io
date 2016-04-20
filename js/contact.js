6fab8cb37d4641672f8483cf0d2437a3c2285fd6
Matheus Pereira <matheper@gmail.com>
2016-04-19 23:50:44 -0300

    Added market contact backend.

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,5 @@
 $(function() {
-    $('form#contact').on('submit', function (e) {
+    $('form#usercontact').on('submit', function (e) {
         e.preventDefault();
         var formData = JSON.stringify(
             {'mail': $(this).find('#mail').val()}
@@ -7,6 +7,28 @@ $(function() {
         $.ajax({
             type: 'POST',
             url: 'http://contact-aomercado.rhcloud.com/contact',
+            contentType: "application/json",
+            dataType: 'json',
+            data: formData,
+            success: function(json) {
+                $(".form-section.bgcontato #contact").toggle();
+                $(".form-section.bgcontato #invite-message").toggle();
+                $(".form-section.bgcontato #contact-response").toggle();
+            },
+        });
+    });
+    $('form#marketcontact').on('submit', function (e) {
+        e.preventDefault();
+        var formData = JSON.stringify({
+          'name': $(this).find('name').val(),
+          'market': $(this).find('#market').val(),
+          'mail': $(this).find('#mail').val(),
+          'phone': $(this).find('phone').val(),
+          'message': $(this).find('message').val(),
+        });
+        $.ajax({
+            type: 'POST',
+            url: 'http://contact-aomercado.rhcloud.com/market',
             contentType: "application/json",
             dataType: 'json',
             data: formData,
